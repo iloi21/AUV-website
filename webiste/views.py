56ea5a7faef1773056f78deb3856cdf7d31da50d
Samuel Gonzalez <samtristan11@gmail.com>
2025-11-13 15:30:18 -0800

    Update views.py

--- webiste/views.py
+++ webiste/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-views = Blueprint('views', __name__)
+views = Blueprint('views', __name__, template_folder='template')
 
 @views.route('/home')
 def home():
