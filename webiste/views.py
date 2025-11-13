40024cfdcfcbb9ec582ea4a68edc3b14c56c43c9
Samuel Gonzalez <samtristan11@gmail.com>
2025-11-13 14:28:26 -0800

    Add views for home, timeline, members, and sponsors

---
+++ webiste/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/home')
+def home():
+    return render_template('home.html')
+
+@views.route('/timeline')
+def timeline():
+    return render_template('timeline.html')
+
+@views.route('/members')
+def members():
+    return render_template('members.html')
+
+@views.route('/sponsors')
+def sponsors():
+    return render_template('sponsors.html')
